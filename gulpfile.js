37a2911023d6caa6f9bf0c6ff0629422a1393b2f
Bruno Farias <begfarias@gmail.com>
2017-09-30 11:57:38 -0300

    Atualizando projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,83 @@
-var gulp        = require('gulp');
-    uglify      = require('gulp-uglify');
-    less        = require('gulp-less');
-    concat      = require('gulp-concat');
-    minifyCSS   = require('gulp-minify-css');
-    svgstore    = require('gulp-svgstore');
-    svgmin      = require('gulp-svgmin');
-    path        = require('path');
-    htmlmin     = require('gulp-htmlmin');
-    fileinclude = require('gulp-file-include');
-    myip        = require('quick-local-ip');
-    connect     = require('gulp-connect');
-    clean       = require('gulp-clean');
+var gulp      = require('gulp');
+    uglify    = require('gulp-uglify'),
+    less      = require('gulp-less'),
+    concat    = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    svgstore  = require('gulp-svgstore'),
+    svgmin    = require('gulp-svgmin'),
+    myip      = require('quick-local-ip'),
+    connect   = require('gulp-connect'),
+    clean     = require('gulp-clean'),
+    imagemin  = require('gulp-imagemin');
 
 function swallowError (error) {
-
-    //If you want details of the error in the console
     console.log(error.toString());
-
     this.emit('end');
 }
 
 // GERAR SVG
 
-// limpando svg
-gulp.task('clean-svg', function () {
-  return gulp.src('assets/images/svg/svg.svg', {read: false})
-    .pipe(clean());
+    // limpando svg
+    gulp.task('clean-svg', function () {
+      return gulp.src('assets/images/svg/svg.svg', {read: false})
+        .pipe(clean());
+    });
+
+    // gerando svg
+    gulp.task('svg', ['clean-svg'], function () {
+        return gulp.src('assets/images/svg/**/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(gulp.dest('assets/images/svg'))
+    });
+
+// OTIMIZANDO IMAGENS
+gulp.task('images', function() {
+    return gulp.src([
+        'assets/images/**/*.gif',
+        'assets/images/**/*.jpg',
+        'assets/images/**/*.png'
+    ])
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5})
+    ]))
+    .pipe(gulp.dest('assets/images/'))
 });
 
-// gerando svg
-gulp.task('svg', ['clean-svg'], function () {
-    return gulp.src('assets/images/svg/**/*.svg')
-    .pipe(svgmin(function (file) {
-        var prefix = path.basename(file.relative, path.extname(file.relative));
-        return {
-            plugins: [{
-                cleanupIDs: {
-                    prefix: prefix + '-',
-                    minify: true
-                }
-            }]
-        }
-    }))
-    .pipe(svgstore())
-    .pipe(gulp.dest('assets/images/svg'))
-});
-
-//File Include
-gulp.task('fileinclude', function() {
-    gulp.src(['_matriz/html/**/index.html'])
-    .pipe(fileinclude({
-        prefix: '@@',
-        basepath: '@root'
-    }))
-    .pipe(gulp.dest('_matriz/build/'))
-    .pipe(connect.reload());
-});
+// HTML RELOAD
 gulp.task('htmlreload',function(){
-    gulp.src('_matriz/html/**/*.html')
-    .pipe(connect.reload());
-});
-
-// MINIFICAR HTML
-gulp.task('html', function() {
-    return gulp.src('assets/html/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true,minifyJS: true}))
-    .pipe(gulp.dest('assets/html/'))
+    gulp.src('assets/*.html')
     .pipe(connect.reload());
 });
 
 // OTIMIZAR OS SCRIPTS
 var global = [
     'assets/js/plugins/jquery.js',
+    'assets/js/plugins/mask.js',
     'assets/js/plugins/modernizr.js',
-    'assets/js/plugins/svg4everybody.js',
-    'assets/js/plugins/masonry.js',
-    'assets/js/plugins/imagesloaded.js',
-    'assets/js/plugins/classie.js',
-    'assets/js/plugins/AnimOnScroll.js',
-    'assets/js/plugins/waypoints.js'
+    'assets/js/plugins/owl.carousel.js',
+    'assets/js/plugins/placeholder.js',
+    'assets/js/plugins/validate.js',
+    'assets/js/modules/main.js'
 ];
 
 gulp.task('scripts', function() {
     gulp.src(global)
-    .pipe(concat("plugins.js"))
+    .pipe(concat("scripts.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('./assets/js/plugins/'));
+    .pipe(gulp.dest('assets/js'));
 });
 
 gulp.task('connect', function() {
@@ -100,15 +92,14 @@ gulp.task('less', function() {
     .pipe(less())
     .on('error', swallowError)
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./assets/css'))
+    .pipe(gulp.dest('assets/css'))
     .pipe(connect.reload());
 });
 
 // WATCH LESS, SCRIPTS E LIVERELOAD
 gulp.task('watch', function() {
-    gulp.watch(['_matriz/html/**/*.html', '!_matriz/html/**/*min.html'], ['htmlreload', 'fileinclude']);
-    gulp.watch(['assets/html/**/*.html', '!assets/html/**/*min.html'], ['html']);
+    gulp.watch('assets/*.html', ['htmlreload']);
     gulp.watch('assets/less/**/*.less', ['less']);
 });
 
-gulp.task('default', ['less', 'fileinclude', 'scripts', 'svg', 'connect', 'watch']);
+gulp.task('default', ['less', 'scripts', 'svg', 'connect', 'watch']);
